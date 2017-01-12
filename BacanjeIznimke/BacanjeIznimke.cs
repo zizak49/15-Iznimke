59ed7e72c64905b9b342caed99a1e4c5e76a6d3b
Mislav Franjo <mislav.franja@gmail.com>
2017-01-12 20:36:35 +0100

    1

--- BacanjeIznimke/BacanjeIznimke.cs
+++ BacanjeIznimke/BacanjeIznimke.cs
@@ -6,13 +6,22 @@ namespace Vsite.CSharp
     {
         public static int Faktorjel(int broj)
         {
-            // TODO: Dodati u metodu provjeru je li argument manji od 0 i u tom slučaju baciti iznimku tipa ArgumentOutOfRangeException s odogovarajućom porukom
-
-            int rezultat = 1;
-            for (int i = 2; i <= broj; ++i)
-                rezultat *= i;
-            return rezultat;
+            //  Dodati u metodu provjeru je li argument manji od 0 i u tom slučaju baciti iznimku tipa ArgumentOutOfRangeException s odogovarajućom porukom
+            if (broj < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(broj), "Argumnt ne smoje bi manji od 0");
+            }
+            try
+            {
+                int rezultat = 1;
+                for (int i = 2; i <= broj; ++i)
+                    rezultat *= i;
+                return rezultat;
+            }
+            catch (OverflowException e) { new ArgumentOutOfRangeException("Argument je prevelik", e); }
+            return 0;
         }
+    
 
         public static int Povrh(int n, int k)
         {
@@ -25,18 +34,20 @@ namespace Vsite.CSharp
         static void Main(string[] args)
         {
             // TODO: Provjeriti vraćaju li donji pozivi metode očekivane rezultate
+            try
+            {
+                int n = 0;
+                Console.WriteLine("{0}! = {1}", n, Math.Faktorjel(n)); // trebalo bi biti 0! = 1
+                n = 3;
+                Console.WriteLine("{0}! = {1}", n, Math.Faktorjel(n)); // trebalo bi biti 3! = 6
+                n = 5;
+                Console.WriteLine("{0}! = {1}", n, Math.Faktorjel(n)); // trebalo bi biti 5! = 120
 
-            int n = 0;
-            Console.WriteLine("{0}! = {1}", n, Math.Faktorjel(n)); // trebalo bi biti 0! = 1
-            n = 3;
-            Console.WriteLine("{0}! = {1}", n, Math.Faktorjel(n)); // trebalo bi biti 3! = 6
-            n = 5;
-            Console.WriteLine("{0}! = {1}", n, Math.Faktorjel(n)); // trebalo bi biti 5! = 120
-
-            n = -1;
-            Console.WriteLine("{0}! = {1}", n, Math.Faktorjel(n)); // trebalo bi baciti iznimku!
-
-
+                n = -1;
+                Console.WriteLine("{0}! = {1}", n, Math.Faktorjel(n)); // trebalo bi baciti iznimku!
+            }
+            catch (ArgumentOutOfRangeException e) { Console.WriteLine(e); }
+            
             Console.WriteLine("GOTOVO!!!");
             Console.ReadKey();
         }
